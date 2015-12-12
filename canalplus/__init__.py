ddba62b952b1fb6b6251bfdfa565244defaa0d85
desbma <desbma@users.noreply.github.com>
2015-12-12 17:08:22 +0100

    Better download error handling

--- canalplus/__init__.py
+++ canalplus/__init__.py
@@ -91,35 +91,40 @@ class CanalPlusVideo(CanalPlusApiObject):
       logging.getLogger().info("File already exists, skipping download")
       return
 
-    with mkstemp_ctx.mkstemp(suffix=".ts") as video_filepath_tmp:
-      # download
-      if sys.stdout.isatty() and logging.getLogger().isEnabledFor(logging.INFO):
-        progress = progress_display.ProgressBar()
-      else:
-        progress = None
-      if self.stream_url.endswith(".m3u8"):
-        # fetch m3u8 playlist
-        m3u8_data = self.fetchText(self.stream_url)
-        # parse it
-        ts_urls = tuple(filter(lambda x: not x.startswith("#"),
-                               m3u8_data.splitlines()))
-        # download ts files
-        self.download_ts(ts_urls, video_filepath_tmp, progress)
+    try:
+      with mkstemp_ctx.mkstemp(suffix=".ts") as video_filepath_tmp:
+        # download
+        if sys.stdout.isatty() and logging.getLogger().isEnabledFor(logging.INFO):
+          progress = progress_display.ProgressBar()
+        else:
+          progress = None
+        if self.stream_url.endswith(".m3u8"):
+          # fetch m3u8 playlist
+          m3u8_data = self.fetchText(self.stream_url)
+          # parse it
+          ts_urls = tuple(filter(lambda x: not x.startswith("#"),
+                                 m3u8_data.splitlines()))
+          # download ts files
+          self.download_ts(ts_urls, video_filepath_tmp, progress)
 
-      else:
-        # direct stream download
-        logging.getLogger().info("Downloading video to '%s'..." % (video_filepath_ts))
-        self.download_ts((self.stream_url,), video_filepath_tmp, progress)
+        else:
+          # direct stream download
+          logging.getLogger().info("Downloading video to '%s'..." % (video_filepath_ts))
+          self.download_ts((self.stream_url,), video_filepath_tmp, progress)
 
-      if progress is not None:
-        progress.updateProgress(100)
-        progress.display()
-        progress.end()
+        if progress is not None:
+          progress.updateProgress(100)
+          progress.display()
+          progress.end()
 
-      # try to remux to mp4
-      remuxed = self.remuxToMp4(video_filepath_tmp, video_filepath_mp4)
-      if not remuxed:
-        shutil.move(video_filepath_tmp, video_filepath_ts)
+        # try to remux to mp4
+        remuxed = self.remuxToMp4(video_filepath_tmp, video_filepath_mp4)
+        if not remuxed:
+          shutil.move(video_filepath_tmp, video_filepath_ts)
+
+    except Exception as e:
+      logging.getLogger().error("Download failed: %s %s", e.__class__.__qualname__, e)
+      exit(1)
 
   def download_ts(self, urls, filepath, progress):
     """ Download one or several MPEG-TS videos to a file. """
